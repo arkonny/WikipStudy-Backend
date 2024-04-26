fbeb19a2579520cf7276f3fe4a28f583c22321a4
Arkonny <arkonny.s@gmail.com>
2024-04-26 18:12:07 +0300

    Quiz, Question, Result types

---
+++ src/types/DBTypes.ts
@@ -0,0 +1,61 @@
+import {Document, Types} from 'mongoose';
+
+type User = Partial<Document> & {
+  id: Types.ObjectId | string;
+  user_name: string;
+  email: string;
+  role: 'user' | 'admin';
+  password: string;
+};
+
+type UserOutput = Omit<User, 'password' | 'role'>;
+
+type UserInput = Omit<User, 'id' | 'role'>;
+
+type UserTest = Partial<User>;
+
+type LoginUser = Omit<User, 'password'>;
+
+type TokenContent = {
+  token: string;
+  user: LoginUser;
+};
+
+// Separate the types for the Quiz and Question entities
+type Quiz = Partial<Document> & {
+  id: Types.ObjectId | string;
+  quiz_name: string;
+  questions: [Types.ObjectId | Question];
+  owner: Types.ObjectId | User;
+  filename: string;
+};
+
+type QuizTest = Partial<Quiz>;
+
+// A question can be of different types
+type Question = {
+  question: string;
+  type: 'multiple' | 'single';
+  options?: string[];
+  answers: string[];
+};
+
+type Result = {
+  id: Types.ObjectId | string;
+  quiz: Types.ObjectId | Quiz;
+  user: Types.ObjectId | User;
+  score: number;
+};
+
+export {
+  User,
+  UserOutput,
+  UserInput,
+  UserTest,
+  LoginUser,
+  Quiz,
+  QuizTest,
+  Question,
+  Result,
+  TokenContent,
+};

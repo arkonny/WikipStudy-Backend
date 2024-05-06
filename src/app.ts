fc9f6c41d5053c889941a30f16c61e604ab21572
Arkonny <arkonny.s@gmail.com>
2024-05-06 14:33:26 +0300

    Not authorised error fix

--- src/app.ts
+++ src/app.ts
@@ -35,6 +35,7 @@ app.use(
 
 (async () => {
   try {
+    /*
     // TODO Create a rate limit rule instance (not WSK2 course)
     const rateLimitRule = createRateLimitRule({
       identifyContext: (ctx) => ctx.id,
@@ -46,6 +47,7 @@ app.use(
         login: rateLimitRule({window: '1m', max: 5}),
       },
     });
+    */
 
     const schema_executable = constraintDirective()(
       makeExecutableSchema({
@@ -54,7 +56,9 @@ app.use(
       }),
     );
 
-    const schema = applyMiddleware(schema_executable, permissions);
+    // /!\ For when shield is fixed /!\
+    //const schema = applyMiddleware(schema_executable, permissions);
+    const schema = applyMiddleware(schema_executable);
 
     const server = new ApolloServer<MyContext>({
       schema,
@@ -67,6 +71,11 @@ app.use(
           : ApolloServerPluginLandingPageLocalDefault(),
       ],
       includeStacktraceInErrorResponses: false,
+      formatError: (formattedError, error) => {
+        console.log('error:', error);
+        console.log('formattederror:', formattedError);
+        return formattedError;
+      },
     });
     await server.start();
 
